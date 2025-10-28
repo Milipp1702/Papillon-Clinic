748a71e5ff41dfc96a6b2da52ec46797095b46a0
Milena Petersen <milenappetersen17@gmail.com>
2025-10-27 23:37:22 -0300

    Replacing JavaMailSender for Resender API

--- papillon-clinic-api/src/main/java/br/edu/ifrs/canoas/papillon_clinic/service/EmailService.java
+++ papillon-clinic-api/src/main/java/br/edu/ifrs/canoas/papillon_clinic/service/EmailService.java
@@ -1,5 +1,9 @@
 package br.edu.ifrs.canoas.papillon_clinic.service;
 
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+import com.resend.services.emails.model.CreateEmailResponse;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,27 +21,32 @@ public class EmailService {
     @Value("${app.url}")
     private String appUrl;
 
-    public void sendResetPasswordEmail(String destinatario, String token) throws MessagingException {
+    @Value("${spring.mail.username}")
+    private String apiKey;
+
+    public void sendResetPasswordEmail(String addressee, String token) throws MessagingException {
+        Resend resend = new Resend(apiKey);
+
         String link = appUrl + "reset-password/?token=" + token;
+        String htmlContent = "<p>Olá,</p>" +
+                "<p>Recebemos uma solicitação para redefinir sua senha.</p>" +
+                "<p><a href=\"" + link + "\">Clique aqui para redefinir sua senha</a></p>" +
+                "<p>Este link é válido por 1 hora.</p>" +
+                "<p>Se você não solicitou essa alteração, ignore este e-mail.</p>" +
+                "<br><p>Atenciosamente,<br>Equipe Clínica Papillon</p>";
 
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from("Papillon Clinic <papillon_clinic@resend.dev>")
+                .to(addressee)
+                .subject("Redefinição de Senha – Clínica Papillon")
+                .html(htmlContent)
+                .build();
 
-            helper.setTo(destinatario);
-            helper.setSubject("Redefinição de Senha – Clínica Papillon");
-            helper.setFrom("papillon@resend.dev");
-
-
-            String htmlContent = "<p>Olá,</p>" +
-                    "<p>Recebemos uma solicitação para redefinir sua senha.</p>" +
-                    "<p><a href=\"" + link + "\">Clique aqui para redefinir sua senha</a></p>" +
-                    "<p>Este link é válido por 1 hora.</p>" +
-                    "<p>Se você não solicitou essa alteração, ignore este e-mail.</p>" +
-                    "<br><p>Atenciosamente,<br>Equipe Clínica Papillon</p>";
-
-            helper.setText(htmlContent, true);
-
-            mailSender.send(message);
+        try {
+            CreateEmailResponse data = resend.emails().send(params);
+        } catch (ResendException e) {
+            e.printStackTrace();
+        }
     }
 }
 
